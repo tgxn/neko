e8273d3691b9cea96f7d8d3487d545a4de0a2e6a
tgxn <tgxn@desktops.tgxn.int>
2026-02-08 19:16:05 +0800

    revert slider

--- client/src/components/settings.vue
+++ client/src/components/settings.vue
@@ -4,7 +4,7 @@
       <li>
         <span>{{ $t('setting.scroll') }}</span>
         <label class="slider">
-          <input type="range" min="0" max="1" v-model="scroll" />
+          <input type="range" min="1" max="100" v-model="scroll" />
         </label>
       </li>
       <li>
