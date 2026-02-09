3717650b95f4859c5b3eb5616183ebd6d24da92c
tgxn <tgxn@desktops.tgxn.int>
2026-02-09 15:57:03 +0800

    branding

--- client/src/components/connect.vue
+++ client/src/components/connect.vue
@@ -2,8 +2,8 @@
   <div class="connect">
     <div class="window">
       <div class="logo" title="About n.eko" @click.stop.prevent="about">
-        <img src="@/assets/images/logo.svg" alt="n.eko" />
-        <span><b>n</b>.eko</span>
+        <img src="@/assets/images/logo.svg" alt="n.eko" width="35px" />
+        <span><b>neko</b>.tgxn.net</span>
       </div>
       <form class="message" v-if="!connecting" @submit.stop.prevent="connect">
         <span v-if="!autoPassword">{{ $t('connect.login_title') }}</span>
